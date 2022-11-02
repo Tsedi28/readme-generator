32a6661926bd3de9f2313f35cf694e344ab4e2f8
Tsedi28 <tsedi22@gmail.com>
2022-11-01 21:04:59 -0400

    Add license

---
+++ dist/README.md
@@ -0,0 +1,8 @@
+
+# TEST 4
+
+[![GitHub license](https://img.shields.io/badge/license-GNU-blue.svg)] 
+
+## Discription
+README.md
+    
--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require ("inquirer")
+const fs = require ("fs")
 // TODO: Create an array of questions for user input
 const questions = [];
+inquirer.prompt([
+    {
+        type:"input",
+        message:"what is the title?", 
+        name : "title" 
+    },
+    {
+        type:"input",
+        message:"what is the discription?", 
+        name : "discription" 
+    },
+    {
+        type:"list",
+        message:"Which license does this project use?", 
+        name : "license",
+        choices:["MIT", "APACHE", "GNU", "NONE"]
+    }
+]).then(answers => {
+    fs.writeFileSync("./dist/README.md",`
+# ${answers.title}
 
+[![GitHub license](https://img.shields.io/badge/license-${answers.license}-blue.svg)] 
+
+## Discription
+${answers.discription}
+    `)
+})
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
