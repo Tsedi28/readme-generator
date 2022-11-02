ab71fb4232391f0f74be74289ac8da47854bc78f
Tsedi28 <tsedi22@gmail.com>
2022-11-01 22:04:52 -0400

    index.js update

--- dist/README.md
+++ dist/README.md
@@ -1,8 +1,33 @@
+# l
+![GitHub license](https://img.shields.io/badge/license-APACHE-yellowgreen.svg)
+## Github URL 🌐
+[k](https://github.com/k/)
+## Description 📝
+undefined
+## Table of Contents 🗒
+* [Installations](#dependencies)
+* [Usage](#usage)
 
-# TEST 4
+* [License](#license)
 
-[![GitHub license](https://img.shields.io/badge/license-GNU-blue.svg)] 
-
-## Discription
-README.md
-    
+* [Contributors](#contributors)
+* [Test](#test)
+## Installations (Dependencies) 💻
+To install dependencies, run these commands:
+```
+npm i
+```
+## Usage 🏆
+oo
+## License 📛
+      Copyright © APACHE. All rights reserved. 
+      
+      Licensed under the APACHE license.
+## Contributors 😃
+Tsedenia Bogale
+Contact me at k
+## Tests 🧪
+To run tests, run these commands:
+```
+npm test
+```
--- index.js
+++ index.js
@@ -1,40 +1,81 @@
 // TODO: Include packages needed for this application
-const inquirer = require ("inquirer")
-const fs = require ("fs")
-// TODO: Create an array of questions for user input
-const questions = [];
-inquirer.prompt([
+// Dependencies
+const inquirer = require ("inquirer");
+const fs = require ("fs");
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown.js");
+
+// Array of questions for user to generate README.md
+const questions = [
+
     {
         type:"input",
-        message:"what is the title?", 
-        name : "title" 
+        message:"What's your Github username?", 
+        name : "github" 
     },
     {
         type:"input",
-        message:"what is the discription?", 
+        message:"What's your email?", 
+        name : "email" 
+    },
+    {
+        type:"input",
+        message:"what is the name of your project?", 
+        name : "title" 
+    },
+
+    {
+        type:"input",
+        message:"Describe about your project:", 
         name : "discription" 
+    },
+    {
+        type:"input",
+        message:"what is the installation instructions?", 
+        name : "installation instructions" 
     },
     {
         type:"list",
         message:"Which license does this project use?", 
         name : "license",
         choices:["MIT", "APACHE", "GNU", "NONE"]
+    },
+    {
+        type: "input",
+        name: "dependencies",
+        message: "Any dependencies to install?",
+        default: "npm i"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "What command should be run to run tests?",
+        default: "npm test"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What does the usage of this repo?",
+    },
+    {
+        type: "input",
+        name: "contributors",
+        message: "Who are the contributors of this repo?",
     }
-]).then(answers => {
-    fs.writeFileSync("./dist/README.md",`
-# ${answers.title}
+];
 
-[![GitHub license](https://img.shields.io/badge/license-${answers.license}-blue.svg)] 
+// function to write README file
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
-## Discription
-${answers.discription}
-    `)
-})
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
+// function to initialize program
+function init() {
+    inquirer.prompt(questions)
+        .then((inquirerAnswers) => {
+            console.log("Generating.... Please wait....");
+            writeToFile("./dist/README.md", generateMarkdown({ ...inquirerAnswers }));
+        })
+}
 // Function call to initialize app
 init();
